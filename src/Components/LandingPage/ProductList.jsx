b9a3a488348a28ba112bd00c90f534d8c7fc1a4a
antinoKartik <kartik.c@antino.io>
2024-01-11 23:20:11 +0530

    deployement check

--- src/Components/LandingPage/ProductList.jsx
+++ src/Components/LandingPage/ProductList.jsx
@@ -4,7 +4,7 @@ import ProductCard from "./ProductCard";
 const ProductList = ({ productList }) => {
   return (
     <div>
-      <div className="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-8 sm:justify-start justify-center mt-16 px-20 ">
+      <div className="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-8 sm:justify-start justify-center mt-16 px-16 ">
         {productList?.map((e) => {
           return (
             <ProductCard
